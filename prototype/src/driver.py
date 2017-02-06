dbd0cfaa682e53c23bd52d6a713efba385b3c397
shreyasrkarthik <shreyas.rkarthik@gmail.com>
2017-02-06 22:09:51 +0530

    "Driver code working, threshold has to be changed"

--- prototype/src/driver.py
+++ prototype/src/driver.py
@@ -1,15 +1,30 @@
-import FeatureExtractor as fe
+from FeatureExtractor import FeatureExtractor as fe
 import operator
+from nltk.tokenize import sent_tokenize
+from nltk import word_tokenize
+
 
 class Driver:
-    def getTokens(self, line):
-        return line.split(' ')
-
+    
     def getThreshold(self, featureValuesDict):
         featureValues = list(featureValuesDict.values())
         return sum(featureValues) / float(len(featureValues))
 
-    def driver(self, filePath):
+    def read_sentences(self, filepath, chunk_size=10240):
+        last = ""
+        with open(filepath) as inp:
+            while True:
+                buf = inp.read(chunk_size)
+                buf = ''.join([i if ord(i) < 128 else ' ' for i in buf])
+                if not buf:
+                    break
+                sentences = sent_tokenize(last + buf)
+                last = sentences.pop()
+                for sentence in sentences:
+                    yield sentence
+            yield last
+
+    def driver(self, filepath):
         weights = {'SWP': 0.2, 'NOWT': 0.4, 'NNP': 0.3, 'NVP': 0.1}
 
         SWPValues = {}
@@ -17,21 +32,21 @@ class Driver:
         NNPValues = {}
         NVPValues = {}
 
-        currentLine = 1
+        currentSent = 1
         title = "Process"
-        finalTextLines = []
+        sentences = []
+        filteredSentences = []
 
-        with open(filePath, 'r') as fp:
-            for line in fp:
-                print line
-                tokenArray = self.getTokens(line)
-                SWPValues[currentLine] = fe.getStopWordsPerc(tokenArray)
-                NOWTValues[currentLine] = fe.getNumOverlappingWords(title, line)
-                NNPValues[currentLine] = fe.getNumNounPhrases(tokenArray)
-                NVPValues[currentLine] = fe.getNumVerbPhrases(tokenArray)
-                currentLine += 1
+        for sentence in self.read_sentences(filepath):
+            sentences.append(sentence)
+            tokenArray = word_tokenize(sentence)
+            SWPValues[currentSent] = fe().getStopWordsPerc(tokenArray)
+            NOWTValues[currentSent] = fe().getNumOverlappingWords(title, sentence)
+            NNPValues[currentSent] = fe().getNumNounPhrases(tokenArray)
+            NVPValues[currentSent] = fe().getNumVerbPhrases(tokenArray)
+            currentSent += 1
 
-        totalLines = currentLine
+        totalSent = currentSent
 
         SWPThreshold = self.getThreshold(SWPValues)
         NOWTThreshold = self.getThreshold(NOWTValues)
@@ -39,21 +54,21 @@ class Driver:
         NVPThreshold = self.getThreshold(NVPValues)
 
 
-        for i in range(1,totalLines):
-            if((SWPValues[currentLine] <= SWPThreshold) and (NOWTValues[currentLine] >= NOWTThreshold) and (NNPValues[currentLine] >= NNPThreshold) and (NVPValues[currentLine]) >= NVPThreshold):
-                    finalTextLines.append(i)
+        for i in range(1, totalSent):
+            if((SWPValues[i] <= SWPThreshold) and (NOWTValues[i] >= NOWTThreshold) and (NNPValues[i] >= NNPThreshold) and (NVPValues[i]) >= NVPThreshold):
+                    filteredSentences.append(sentences[i])
+        print filteredSentences
 
-        # scoreThreshold = 50
         LinesScore = {}
-        for lineNo in finalTextLines:
-            score = ((SWPValues[lineNo] * weights['SWP']) + (NOWTValues[lineNo] * weights['NOWT']) + (NNPValues[lineNo] * weights['NNP']) + (NVPValues[lineNo] * weights['NNP']))
-            LinesScore[lineNo] = score
+        # for sentNum in filteredSentences:
+        #     score = ((SWPValues[sentNum] * weights['SWP']) + (NOWTValues[sentNum] * weights['NOWT']) + (NNPValues[sentNum] * weights['NNP']) + (NVPValues[sentNum] * weights['NNP']))
+        #     LinesScore[sentNum] = score
+        #
+        # SortedDict = sorted(LinesScore.items(), key=operator.itemgetter(0))
+        # return SortedDict
 
-        SortedDict = sorted(LinesScore.items(), key=operator.itemgetter(1))
-        return SortedDict
 
 if __name__ == '__main__':
     d = Driver()
-    print d.driver("sample_3_1.txt")
-
+    print(d.driver("sample_3_1.txt"))
 
