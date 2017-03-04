bb7046895a26415da8f69b3d52b3ec5b6e49509d
SudharshanaSL <15.hari.hari@gmail.com>
2017-03-05 03:03:50 +0530

    Added one observation as comment

--- prototype/SlideGenerator/SlideGenerator.py
+++ prototype/SlideGenerator/SlideGenerator.py
@@ -3,11 +3,14 @@ from pptx.util import Inches, Pt
 
 """
 there are already APIs for levelling so not a problem
-will look into this : http://pbpython.com/creating-powerpoint.html
+will look into these :
+http://pbpython.com/creating-powerpoint.html
+http://python-pptx.readthedocs.io/en/latest/user/quickstart.html
 """
 
 def setFooter(from_slide_number,to_slide_number,slide_ref,footer_text):
     for slide_number in range(from_slide_number,to_slide_number+1):
+        # position reference from top left corner
         slide = slide_ref[slide_number]
         left = Inches(2.8)
         width = Inches(3.7)
@@ -15,6 +18,7 @@ def setFooter(from_slide_number,to_slide_number,slide_ref,footer_text):
         height = Inches(1)
         text_box = slide.shapes.add_textbox(left, top, width, height)
         text_box_frame = text_box.text_frame
+        # need to look into removal of unnecessarily new line character created in text box
         footer_paragraph = text_box_frame.add_paragraph()
         footer_paragraph.text = footer_text
         footer_paragraph.font.bold = True
